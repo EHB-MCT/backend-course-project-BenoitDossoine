8025432788dbd1ac3338e023f0a9f32436057d63
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-26 21:27:15 +0200

    Creates Team model and migration
    Creates Team model and migration
    Seeder updated to insert dummy data

--- gamification-BD/database/seeders/DatabaseSeeder.php
+++ gamification-BD/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +18,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name = "Benoit";
+        $user->email = "benoit@dossoine.be";
+        $user->password = Hash::make("student");
+        $user->save();
+
+        $user = new User();
+        $user->name = "Mike";
+        $user->email = "mike@derycke.be";
+        $user->password = Hash::make("docent");
+        $user->save();
+
+        $team = new Team();
+        $team->name = "Web 2";
+        $team->description = "This course is given in semester 1.";
+        $team->save();
     }
 }
