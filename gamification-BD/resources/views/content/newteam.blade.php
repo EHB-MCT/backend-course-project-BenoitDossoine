4b3daf19677389034173b98dedbfa7854d2d2795
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-28 21:07:12 +0200

    Style update for buttons and the new team form

--- gamification-BD/resources/views/content/newteam.blade.php
+++ gamification-BD/resources/views/content/newteam.blade.php
@@ -3,15 +3,15 @@
 @section('content')
     <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h1 class="text-xl">Create a new team</h1>
-        <form method="POST" action="{{route('teamcreate')}}">
-            <label for="name">Your team's name</label>
+        <form method="POST" action="{{route('teamcreate')}}" class="flex flex-col w-1/2 mx-auto">
+            <label for="name" class="mt-4">Your team's name</label>
             <input type="text" name="name" id="name">
 
-            <label for="description">What is it about?</label>
-            <input type="textarea" name="description" id="description">
+            <label for="description" class="mt-4">What is it about?</label>
+            <input type="textarea" name="description" id="description" class="h-52">
 
             @csrf
-            <input type="submit">
+            <input type="submit" class="w-1/4 mt-6 btn btn-green self-end">
         </form>
 
     </div>
