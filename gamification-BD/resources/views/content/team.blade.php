bb52b867afe3f2d0afe10fe20d52812d0be1d84a
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-29 14:06:00 +0200

    Authorization to see button create quests
    Adds need for authorization 'create quests' to see the button

--- gamification-BD/resources/views/content/team.blade.php
+++ gamification-BD/resources/views/content/team.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h2 class="text-center uppercase text-5xl">{{$team->name}}</h2>
+        @can('create quests')
         <button class="btn btn-blue"><a href="{{route('newquest',[$team->id])}}">Add quest</a></button>
+        @endcan
         <div class="questContainer grid-cols-3">
             @foreach($quests as $quest)
                 <div class="questTile mt-8 rounded-xl p-6 border-solid border-gray-400 shadow">
