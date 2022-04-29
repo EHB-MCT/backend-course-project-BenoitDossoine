d3604a6a90a5f068f21a1ebe60a8160914c5e336
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-04-29 14:06:59 +0200

    Add authorization for button create team

--- gamification-BD/resources/views/content/teams.blade.php
+++ gamification-BD/resources/views/content/teams.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h1 class="text-xl">These are your teams:</h1>
+        @can('create teams')
         <button class="btn btn-green mt-4"><a href="{{route('newteam')}}">Create new team</a></button>
+        @endcan
         <div class="grid grid-cols-3 gap-4 mt-4">
             @foreach($teamlist as $team)
                 <div class="teamTile border-solid border-2 border-sky-500 p-4 rounded-xl">
