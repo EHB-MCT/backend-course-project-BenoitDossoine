e061c207638177fd73bdb24c41de7409745128d6
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-08 14:04:17 +0200

    Moves logic to add quest from route to controller

--- gamification-BD/app/Http/Controllers/QuestController.php
+++ gamification-BD/app/Http/Controllers/QuestController.php
@@ -1,8 +1,41 @@
 <?php
 namespace App\Http\Controllers;
+use App\Models\Quest;
+use App\Models\Team;
+use Illuminate\Http\Request;
+
 class QuestController extends Controller
 {
     public function newTeamQuest($teamId){
         return view('content.newquest',["teamId"=>$teamId]);
+    }
+
+    public function addTeamQuest(Request $request){
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $experience = $request->input('experience');
+        $teamId = $request->input('teamId');
+//      $module = $request->input('module');
+
+
+        $quest = new Quest();
+        $quest->name=$name;
+        $quest->description=$description;
+        $quest->experience=$experience;
+        $quest->team_id=$teamId;
+        $quest->save();
+
+        $team = Team::find($teamId);
+        $teamUsers = $team->users;
+
+        foreach ($teamUsers as $teamUser)
+        {
+            $achievement = new \App\Models\Achievement();
+            $achievement->status = 'not completed';
+            $achievement->quest_id = $quest->id;
+            $achievement->user_id = $teamUser->id;
+            $achievement->save();
+        }
+        return redirect()->route('team',['team_id'=>$teamId]);
     }
 }
--- gamification-BD/routes/web.php
+++ gamification-BD/routes/web.php
@@ -77,35 +77,7 @@ Route::middleware(['auth'])->group(function(){
         Route::get('/team/{team_id}/newquest', [QuestController::class,"newTeamQuest"])->name('newquest');
 
         //handling of new quest data
-        Route::post('/questcreate', function(Request $request){
-            $name = $request->input('name');
-            $description = $request->input('description');
-            $experience = $request->input('experience');
-            $teamId = $request->input('teamId');
-//          $module = $request->input('module');
-
-
-            $quest = new Quest();
-            $quest->name=$name;
-            $quest->description=$description;
-            $quest->experience=$experience;
-            $quest->team_id=$teamId;
-            $quest->save();
-
-            $team = Team::find($teamId);
-            $teamUsers = $team->users;
-
-            foreach ($teamUsers as $teamUser)
-            {
-                $achievement = new \App\Models\Achievement();
-                $achievement->status = 'not completed';
-                $achievement->quest_id = $quest->id;
-                $achievement->user_id = $teamUser->id;
-                $achievement->save();
-            }
-            return redirect()->route('team',['team_id'=>$teamId]);
-        })->name('questcreate');
-
+        Route::post('/questcreate', [QuestController::class,"addTeamQuest"])->name('questcreate');
 
         //overview of members and possibility to add multiple
         Route::get('/team/{team_id}/members',function($teamId){
