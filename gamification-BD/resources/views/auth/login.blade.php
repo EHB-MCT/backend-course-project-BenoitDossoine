9631cd1de5a208f33d76f580635efc454b6dd6f8
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-18 19:44:54 +0200

    Added glitches

--- gamification-BD/resources/views/auth/login.blade.php
+++ gamification-BD/resources/views/auth/login.blade.php
@@ -37,11 +37,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+
 
                 <x-button class="ml-3">
                     {{ __('Log in') }}
@@ -49,4 +45,8 @@
             </div>
         </form>
     </x-auth-card>
+    <div class="line x1"></div>
+    <div class="line x2"></div>
+    <div class="line x3"></div>
+    <div class="line x4"></div>
 </x-guest-layout>
