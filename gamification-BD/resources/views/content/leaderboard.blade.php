f1bc399feeadfee30dd601fcb47e69eff68119a7
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-18 11:49:52 +0200

    Style update

--- gamification-BD/resources/views/content/leaderboard.blade.php
+++ gamification-BD/resources/views/content/leaderboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <h1 class="text-center uppercase text-6xl font-extralight">Hall of Fame</h1>
+        <h2 class="text-center uppercase text-6xl font-extralight">Hall of Fame</h2>
         <div class="container grid grid-cols-3 gap-4">
             <div class="leaderboardContainer col-span-2">
                 <h2 class="text-3xl subtitle mb-4">Leaderboard</h2>
@@ -25,6 +25,7 @@
                         <p class="">Number of teams</p>
                         <p class="text-right">{{\Illuminate\Support\Facades\Auth::user()->teams->count()}}</p>
                     </div>
+                    @if(Auth::user()->hasRole('member'))
                     <div class="grid grid-cols-2 width-100 justify-between px-6 mx-4 bg-white rounded h-12 items-center">
                         <p class="">Quests achieved</p>
                         <p class="text-right">{{\Illuminate\Support\Facades\Auth::user()->achievements->where('status','completed')->count()}}</p>
@@ -37,6 +38,7 @@
                         <p class="">Quests to complete</p>
                         <p class="text-right">{{\Illuminate\Support\Facades\Auth::user()->achievements->where('status','not completed')->count()}}</p>
                     </div>
+                    @endif
                 </div>
             </div>
         </div>
