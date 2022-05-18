128f84c5998f29b8c9dcba6835b5f75ea67bcdc5
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-18 19:44:47 +0200

    Regrouping of routes for the user_in_team middleware

--- gamification-BD/routes/web.php
+++ gamification-BD/routes/web.php
@@ -56,24 +56,37 @@ Route::middleware(['auth'])->group(function(){
         //handling of new team data
         Route::post('/teamcreate', [TeamController::class,"makeNewTeam"])->name('teamcreate');
 
-        //form to create new quest
-        Route::get('/team/{team_id}/newquest', [QuestController::class,"newTeamQuest"])->name('newquest');
+
 
         //handling of new quest data
         Route::post('/questcreate', [QuestController::class,"addTeamQuest"])->name('questcreate');
 
-        //overview of members and possibility to add multiple
-        Route::get('/team/{team_id}/members',function($teamId){
-            $team = Team::find($teamId);
-            $allUsers = User::role('member')->get();
-            return view('content.teammembers',["team"=>$team, "allUsers"=>$allUsers]);
-        })->middleware('user_in_team:$teamId')->name('teammembers');
 
-        //overview of member's quests to approve
-        Route::get('/team/{team_id}/teamprogress',function($teamId){
-            $team = Team::find($teamId);
-            return view('content.progress',['team'=>$team]);
-        })->name('teamprogress');
+        //middleware to chekc if user is part of the team they want to access
+        Route::middleware('user_in_team:$teamId')->group(function(){
+            //form to create new quest
+            Route::get('/team/{team_id}/newquest', [QuestController::class,"newTeamQuest"])->name('newquest');
+
+            //overview of members and possibility to add multiple members
+            Route::get('/team/{team_id}/members',function($teamId){
+                $team = Team::find($teamId);
+                $allUsers = User::role('member')->get();
+                return view('content.teammembers',["team"=>$team, "allUsers"=>$allUsers]);
+            })->name('teammembers');
+
+            //overview of member's quests to approve
+            Route::get('/team/{team_id}/teamprogress',function($teamId){
+                $team = Team::find($teamId);
+                return view('content.progress',['team'=>$team]);
+            })->name('teamprogress');
+
+            Route::get('/team/{team_id}/editquest/{quest_id}',function($teamId,$questId){
+                $quest = \App\Models\Quest::find($questId);
+                $team = Team::find($teamId);
+                return view('content.editquest',["quest"=>$quest,"team"=>$team]);
+            })->name('editQuestForm');
+        });
+
 
         //handle data to add new members to team
         Route::post('/team/{team_id}/addTeamMembers',[TeamController::class,'addMembers'])->name('addTeamMembers');
@@ -82,12 +95,6 @@ Route::middleware(['auth'])->group(function(){
         Route::post('team/{team_id}/updateAchievement',[AchievementController::class,'updateAchievement'])->name('updateAchievement');
 
         Route::post('team/deleteQuest',[QuestController::class,'deleteQuest'])->name('deleteQuest');
-
-        Route::get('/team/{team_id}/editquest/{quest_id}',function($teamId,$questId){
-            $quest = \App\Models\Quest::find($questId);
-            $team = Team::find($teamId);
-            return view('content.editquest',["quest"=>$quest,"team"=>$team]);
-        })->name('editQuestForm');
 
         Route::post('/editquest', [QuestController::class,"editQuest"])->name('editquest');
     });
